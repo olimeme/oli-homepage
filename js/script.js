cce3038e3e0acbe2015d19ad397d92b584143323
Alibek <alibek.mombekov@mail.ru>
2022-03-29 19:05:34 +0600

    initial commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+var typed = new Typed(".auto-input", {
+  strings: ["Алибек.", "Фронтэндер.", "UX Дизайнер."],
+  typeSpeed: 90,
+  backSpeed: 50,
+  loop: true,
+});
+
+var cursor = document.getElementById("cursor");
+document.onmousemove = function (e) {
+  cursor.style.left = e.pageX + "px";
+  cursor.style.top = e.pageY + "px";
+  cursor.style.display = "block";
+};
