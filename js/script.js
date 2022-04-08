427f78ae16d302372442695cf52fb0261e8f18d3
Alibek <alibek.mombekov@mail.ru>
2022-04-08 11:30:12 +0600

    contact form implemented

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+import { keys } from "./keys.js";
+
+const { HOST, PORT, USERNAME, PASSWORD } = keys;
+const form = document.getElementById("contact_form");
+
 var typed = new Typed(".auto-input", {
   strings: ["Алибек", "Фронтэндер", "UX Дизайнер"],
   typeSpeed: 90,
@@ -5,9 +10,28 @@ var typed = new Typed(".auto-input", {
   loop: true,
 });
 
-var cursor = document.getElementById("cursor");
-document.onmousemove = function (e) {
-  cursor.style.left = e.pageX + "px";
-  cursor.style.top = e.pageY + "px";
-  cursor.style.display = "block";
-};
+function sendEmail() {
+  Email.send({
+    Host: HOST,
+    Port: PORT,
+    Username: USERNAME,
+    Password: PASSWORD,
+    To: "alibek.mombekov@mail.ru",
+    From: document.getElementById("email").value,
+    Subject: "From homepage",
+    Body:
+      "Name: " +
+      document.getElementById("name").value +
+      "<br> Email: " +
+      document.getElementById("email").value +
+      "<br> Phone number: " +
+      document.getElementById("phone").value +
+      "<br> Message: " +
+      document.getElementById("message").value,
+  }).then((message) => alert(message));
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  sendEmail();
+});
